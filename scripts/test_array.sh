eb387ebc521f9c5a5d15ffe725d16e638d62e008
Laia Albors Zumel <laia.albors.3@gmail.com>
2026-06-24 20:03:10 +0200

    Prepare workshop submission

--- scripts/test_array.sh
+++ scripts/test_array.sh
@@ -3,11 +3,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=20:00
-#SBATCH --exclude=gpic09,gpic10
-#SBATCH --output=./logs/test/test_lora_%A_%a.log
-#SBATCH --error=./logs/test/test_lora_%A_%a.log
-#SBATCH --job-name=LoRAArray   # LoRAArray / AnySatArray / SoftConArray / LVDArray / DOFAArray / PanopticonArray
-
+#SBATCH --output=./logs/test/test_alignet_%A_%a.log
+#SBATCH --error=./logs/test/test_alignet_%A_%a.log
+#SBATCH --job-name=ALIGNetArray
 
 set -euo pipefail
 
