644bd02d0fdc8769a7fde979a3893c2e86421d95
Laia Albors Zumel <laia.albors.3@gmail.com>
2026-06-24 10:42:51 +0200

    [NEW/UPD] Add projective model test commands and centralize test scripts in the scripts folder.

--- scripts/test_array.sh
+++ scripts/test_array.sh
@@ -3,9 +3,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=20:00
-#SBATCH --output=./logs/test_alignet_%A_%a.log
-#SBATCH --error=./logs/test_alignet_%A_%a.log
-#SBATCH --job-name=ALIGNetArray
+#SBATCH --exclude=gpic09,gpic10
+#SBATCH --output=./logs/test/test_lora_%A_%a.log
+#SBATCH --error=./logs/test/test_lora_%A_%a.log
+#SBATCH --job-name=LoRAArray   # LoRAArray / AnySatArray / SoftConArray / LVDArray / DOFAArray / PanopticonArray
 
 
 set -euo pipefail
